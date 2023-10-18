3e18a58492505ef39958872dd877aeb18a328442
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2023-10-19 06:46:48 +0900

    wip: zendesk garden

---
+++ notion-integration/src/Form.tsx
@@ -0,0 +1,10 @@
+import { Field, Label, Input } from "@zendeskgarden/react-forms";
+
+export const Form = () => {
+  return (
+    <Field>
+      <Label>Name</Label>
+      <Input />
+    </Field>
+  );
+};
